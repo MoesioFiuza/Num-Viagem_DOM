de48981ce5c83d660f157e2beae6f64ce717445e
Moésio Fiùza <93868671+MoesioFiuza@users.noreply.github.com>
2024-10-14 10:00:45 -0300

    Update NUM VIAGEM.py

--- NUM VIAGEM.py
+++ NUM VIAGEM.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-planilha = r'C:\Users\moesios\Desktop\resolver fumo\BANCO DE DADOS FINAL.xlsx'
+planilha = r'Planilha Entrada'
 df_deslocamento = pd.read_excel(planilha, sheet_name='DESLOCAMENTO')
 
 df_deslocamento['QUE HORAS INICIOU O DESLOCAMENTO'] = pd.to_datetime(df_deslocamento['QUE HORAS INICIOU O DESLOCAMENTO'])
@@ -16,5 +16,5 @@ duplicated_mask = df_deslocamento_sorted.duplicated(subset='ID DOMICÍLIO', keep
 df_deslocamento_sorted.loc[~duplicated_mask, 'VIAGENS_TOTAIS'] = df_deslocamento_sorted['VIAGENS_TOTAIS'].astype('Int64')
 df_deslocamento_sorted.loc[duplicated_mask, 'VIAGENS_TOTAIS'] = '--'
 
-output_file = r'C:\Users\moesios\Desktop\resolver fumo\NUM VIAGEM.xlsx'
+output_file = r'Planilha saída'
 df_deslocamento_sorted.to_excel(output_file, index=False)
